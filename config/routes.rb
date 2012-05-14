2507a67210c0c97bd72249548370fa1c0dd0d260
Rui <rliu200035@gmail.com>
2012-05-13 21:27:16 -0400

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 SampleApp::Application.routes.draw do
   resources :users
-
+  resources :sessions, only: [:new, :create, :destroy]
   root to: 'staticpages#home'
   match '/help', to: 'staticpages#help'
   match '/about', to: 'staticpages#about'
   match '/contact', to: 'staticpages#contact'
   match '/signup', to: 'users#new'
-  
+  match '/signin', to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
   get "staticpages/contact"
   # The priority is based upon order of creation:
   # first created -> highest priority.
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -23,8 +23,12 @@ describe User do
   it {should respond_to(:password)}
   it {should respond_to(:password_confirmation)}
   it {should respond_to(:authenticate)}
+  it {should respond_to(:remember_token)}
   it {should be_valid}
-  
+  describe "remember token" do
+    before {@user.save}
+    its(:remember_token) {should_not be_blank}
+  end
   describe "When name is not present" do
     before {@user.name=" "}
     it {should_not be_valid}
@@ -102,5 +106,6 @@ describe User do
       it { should_not == user_for_invalid_password }
       specify { user_for_invalid_password.should be_false }
     end
+    
   end
 end
